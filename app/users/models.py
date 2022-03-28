6881279774084575c9e2149798f0e95f39170631
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 20:58:56 -0300

    Add a new field in the User model to save the birth date.

--- app/users/models.py
+++ app/users/models.py
@@ -11,6 +11,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
+    birth_date = models.DateField(null=True, blank=True)
 
     objects = UserManager()
 
