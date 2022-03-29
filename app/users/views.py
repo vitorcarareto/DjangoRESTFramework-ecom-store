af1785c98473d0f2d12ee2b46831e9f9cbbfef10
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 21:03:01 -0300

    Create a new endpoint to list the users and also return the age using that property.

--- app/users/views.py
+++ app/users/views.py
@@ -1,5 +1,14 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from app.users.models import User
+from app.users.serializers import UserSerializer
 
 
 class ListUsers(APIView):
-    pass
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
--- store/urls.py
+++ store/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.main.urls')),
+    path('', include('app.users.urls')),
     path('store/', include('app.store.urls')),
 ]
