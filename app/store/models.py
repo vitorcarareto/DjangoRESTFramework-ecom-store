f30e4305ff239ee7749ce4f96066455f78368ded
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 22:06:01 -0300

    Create a new category model and create a relation between the Product and Category models that allow a product to belong to multiple categories.

--- app/store/models.py
+++ app/store/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Products(models.Model):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    products = models.ManyToManyField(Products)
--- app/store/urls.py
+++ app/store/urls.py
@@ -4,6 +4,6 @@ from .views import ListProducts
 app_name = 'store'
 
 urlpatterns = [
-    path('products', ListProducts.as_view()),
+    path('products/', ListProducts.as_view(), name='products'),
     path('products/<int:pk>/', ListProducts.as_view()),
 ]
