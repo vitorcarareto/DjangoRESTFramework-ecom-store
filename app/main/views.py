8979691a7019516d243a1f2af11797b456c703fe
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 22:08:20 -0300

    Create a new decorator to hide a view from logged-in users and allow just AnonymousUsers to access that view.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+
+
+def anonymous_required(redirect_url):
+    def _wrapped(view_func, *args, **kwargs):
+        def check_anonymous(request, *args, **kwargs):
+            view = view_func(request, *args, **kwargs)
+            if request.user.is_authenticated:
+                return redirect(redirect_url)
+            return view
+
+        return check_anonymous
+
+    return _wrapped
 
 
 class Welcome(APIView):
