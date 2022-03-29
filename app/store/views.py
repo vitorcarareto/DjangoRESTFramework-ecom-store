64dd0812e660677d8a2b017c9f9b58af32ce69ea
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 21:09:28 -0300

    Create a new endpoint to create products into the Product model.

--- app/store/views.py
+++ app/store/views.py
@@ -1,5 +1,22 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from app.store.models import Products
+from app.store.serializers import ProductSerializer
 
 
 class ListProducts(APIView):
-    pass
+    allowed_methods = ['GET', 'POST']
+
+    def get(self, request, format=None):
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
