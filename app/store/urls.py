cbab5dcde6f4ea70beac9c677b92075e91438420
vitorcarareto <vitor.carareto@gmail.com>
2022-03-28 21:45:45 -0300

    Create a new endpoint to deactivate/activate a product from the Product model

--- app/store/urls.py
+++ app/store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 
 urlpatterns = [
     path('products', ListProducts.as_view()),
+    path('products/<int:pk>/', ListProducts.as_view()),
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -7,7 +7,7 @@ from app.store.serializers import ProductSerializer
 
 
 class ListProducts(APIView):
-    allowed_methods = ['GET', 'POST']
+    allowed_methods = ['GET', 'POST', 'PATCH']
 
     def get(self, request, format=None):
         products = Products.objects.all()
@@ -16,6 +16,18 @@ class ListProducts(APIView):
 
     def post(self, request, format=None):
         serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        try:
+            product = Products.objects.get(pk=pk)
+        except Products.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ProductSerializer(product, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
